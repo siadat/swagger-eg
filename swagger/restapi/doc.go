40beed4487a838e719eb1617f11c4cbd8784e735
Sina Siadat <siadat@gmail.com>
2017-01-24 16:43:34 +0330

    add generated swagger files

---
+++ swagger/restapi/doc.go
@@ -0,0 +1,19 @@
+/*Package restapi Testman
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /api/v1
+    Version: 0.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
